027b503e19ed7aa28ab8df35e5983df6716f34f6
Ho Wei Chin <hwc.howeichin@gmail.com>
2019-03-27 18:29:07 +0800

    Add files via upload

---
+++ lsm3241_ca2_documentation.sh
@@ -0,0 +1,35 @@
+
+documentation:
+#build yeast and transposon index, also explain why I didn't trim.
+#provide reference to tools used.
+bowtie2-build 'sacCer3.fa,ty5_6p.fa' yeast_trans_index
+
+#alignment of reads to yeast and transposon index:
+export BOWTIE2_INDEXES=$(pwd)/yeast_trans_index
+bowtie2 -x yeast_trans_index -1 ca2_data/A0155657U_1.fq  -2 ca2_data/A0155657U_2.fq -S read_align_to_index.sam
+
+
+#sort the read_align_to_index.sam file
+samtools sort read_align_to_index.sam > sorted_read_align_to_index.sam
+
+#filter the read_align_to_index.sam file based on the negation of :
+#read mapped in proper pair
+#read unmapped 
+#mate unmapped 
+#read fails platform/vendor quality checks 
+#read is PCR or optical duplicate 
+samtools view -F 1550 sorted_read_align_to_index.sam > sorted_filtered_read_align_to_index_latest.sam
+
+F=sorted_filtered_read_align_to_index_latest.sam
+
+grep -i ty5 $F
+
+grep -i ty5 $F | cut -f3,4
+
+grep -i ty5 $F | cut -f3 | uniq -c
+
+grep -i ty5 $F | cut -f3,4 | grep chrXII
+
+
+
+
